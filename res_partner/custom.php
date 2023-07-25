b9f6ba4d3187052e0bd47a6f25384085a86ff4ab
Aditya <aditya@amandjaja.com>
2023-07-25 22:16:49 +0700

    add example to call custom function

---
+++ res_partner/custom.php
@@ -0,0 +1,35 @@
+<?php
+
+	require_once '../vendor/autoload.php';
+	use Laminas\XmlRpc\Client;
+	use Laminas\XmlRpc\Client\Exception\HttpException;
+
+	$url = "http://localhost:8069";
+	$db = "skj_api";
+	$username = "admin";
+	$password = "admin";
+
+	$client = new Client("$url/xmlrpc/2/common");
+	$client->setSkipSystemLookup(true);
+	$uid = $client->call('authenticate', array($db, $username, $password, []));
+	echo '<pre/>';
+	echo 'uid: '.(string)$uid;
+	echo '<pre/>';
+	echo 'custom res partner';
+
+	$models = new Client("$url/xmlrpc/2/object");
+	$models->setSkipSystemLookup(true);
+	$res = $models->call('execute_kw', [
+		$db, 
+		$uid, 
+		$password, 
+		'res.partner', 
+		'test_function', 
+		array(
+			array(
+				'ids'=>array(13),
+			),
+		),
+	]);
+	echo '<pre/>';
+	print_r($res);
