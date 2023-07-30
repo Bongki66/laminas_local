4a49687d094670601745716a4498c2854e37ae6a
Aditya <aditya@amandjaja.com>
2023-07-30 11:23:26 +0700

    add example delete record

---
+++ res_partner/delete.php
@@ -0,0 +1,31 @@
+<?php
+
+	require_once '../vendor/autoload.php';
+	use Laminas\XmlRpc\Client;
+	use Laminas\XmlRpc\Client\Exception\HttpException;
+
+	$url = "http://localhost:8069";
+	$db = "skj_api";
+	$username = "admin";
+	$password = "admin";
+
+	$client = new Client("$url/xmlrpc/2/common");
+	$client->setSkipSystemLookup(true);
+	$uid = $client->call('authenticate', array($db, $username, $password, []));
+	echo '<pre/>';
+	echo 'uid: '.(string)$uid;
+	echo '<pre/>';
+	echo 'delete res partner';
+
+	$models = new Client("$url/xmlrpc/2/object");
+	$models->setSkipSystemLookup(true);
+	$ids = $models->call('execute_kw', [
+		$db, 
+		$uid, 
+		$password, 
+		'res.partner', 
+		'unlink', 
+		[[13]]
+	]);
+	echo '<pre/>';
+	print_r($ids);
